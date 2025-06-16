3ec4869419e1b4f131598b8192c38aae73715f4f
joshuarwood <joshua.randall.wood@gmail.com>
2025-06-15 22:52:32 -0500

    added sector read method that combines streaming and force unit access behavior

--- src/dvdpy/commands.py
+++ src/dvdpy/commands.py
@@ -22,6 +22,10 @@ SPC_INQUIRY    = 0x12
 SBC_START_STOP = 0x1B
 MMC_READ_12    = 0xA8
 
+SECTOR_SIZE = 2048
+RAW_SECTOR_SIZE = 2064
+SECTORS_PER_BLOCK = 16
+
 from . import cextension
 
 def drive_info(fd: int, timeout: int = 1, verbose: bool = False):
@@ -70,5 +74,38 @@ def drive_spin(fd: int, state: bool, timeout: int = 1, verbose: bool = False):
 
     cmd[0] = SBC_START_STOP
     cmd[4] = int(state)
+
+    return cextension.command_device(fd, cmd, buffer, timeout, verbose)
+
+def read_sectors(fd: int, sector: int, sectors: int = SECTORS_PER_BLOCK,
+                 streaming: bool = False, timeout: int = 1, verbose: bool = False):
+    """ Write sectors to the drive cache
+
+    Args:
+        fd (int): file descriptor
+        sector (int): starting sector
+        sectors (int, optional): number of sectors to read (default: 16)
+        streaming (int, optional): use streaming mode when True (default: False)
+        timeout (int, optional): command timeout in seconds (default: 1)
+        verbose (bool, optional): set to True to print more info (default: False)
+
+    Returns:
+        (int): command status (-1 means fail)
+    """
+    cmd = bytearray(12)
+    buffer = bytearray(SECTORS_PER_BLOCK * RAW_SECTOR_SIZE)
+    sectors = SECTORS_PER_BLOCK
+
+    cmd[0] = MMC_READ_12
+    cmd[1] = 0 if streaming else 0x08 # Force Unit Access bit
+    cmd[2] =  (sector & 0xFF000000) >> 24 # sector MSB
+    cmd[3] =  (sector & 0x00FF0000) >> 16
+    cmd[4] =  (sector & 0x0000FF00) >> 8
+    cmd[5] =  (sector & 0x000000FF)       # sector LSB
+    cmd[6] = (sectors & 0xFF000000) >> 24 # sectors MSB
+    cmd[7] = (sectors & 0x00FF0000) >> 16
+    cmd[8] = (sectors & 0x0000FF00) >> 8
+    cmd[9] = (sectors & 0x000000FF)       # sectors LSB
+    cmd[10] = 0x80 if streaming else 0    # streaming bit
 
     return cextension.command_device(fd, cmd, buffer, timeout, verbose)
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 
 import time
 import dvdpy.devices
+import dvdpy.commands
 
 drive = dvdpy.devices.dvd("/dev/sr0")
 print(drive.model_info(True))
 drive.start()
-time.sleep(3)
-drive.stop()
+#time.sleep(3)
+#drive.stop()
+dvdpy.commands.read_sectors(drive.fd, 712980, verbose=True) # replaces read_dummy
+dvdpy.commands.read_sectors(drive.fd, 2295012, verbose=True) # replaces read_dummy
+
+dvdpy.commands.read_sectors(drive.fd, 80, streaming=True, verbose=True)
+dvdpy.commands.read_sectors(drive.fd, 0, streaming=True, verbose=True)
